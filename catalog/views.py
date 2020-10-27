27a2376ce1bd677a5bac7ecad39954b5b8488bb9
JanSoLul <jiwoong0218@gmail.com>
2020-10-27 13:32:45 -0700

    User authentication and permissions

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from catalog.models import Book, Author, Language, Bookinstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def index(request):
@@ -73,3 +74,13 @@ class AuthorDetailView(generic.DetailView):
     def author_detail_view(request, primary_key):
         author = get_object_or_404(Author, pk=primary_key)
         return render(request, 'catalog/author_detail.html', context={'author': author})
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    # Generic class-based view listing books on load to current user.
+    model = Bookinstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginated_by = 10
+
+    def get_queryset(self):
+        return Bookinstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Redirect to home URL after login (Default redirects to accounts/profile)
+LOGIN_REDIRECT_URL = '/'
+
 
 # Application definition
 
@@ -122,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
