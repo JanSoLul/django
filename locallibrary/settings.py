c0d511b567b4f29d0c11caac7f5acb207bddb647
JanSoLul <jiwoong0218@gmail.com>
2020-11-13 11:58:16 -0800

    remove ignored file

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fu6c&z%=^e)uhxz2u=1!2^a99iqxzuz#n4q%6l51mb@km_#v+u'
+# SECRET_KEY = 'fu6c&z%=^e)uhxz2u=1!2^a99iqxzuz#n4q%6l51mb@km_#v+u'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
